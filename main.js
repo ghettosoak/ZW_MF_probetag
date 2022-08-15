e681acbad2e9c1e17ee92efdde76ddd52f387da0
Mike Fischer <e@ject.ch>
2022-08-15 11:09:50 +0200

    adds list mechanism, less support

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+import './main.less'
 
 let orgID = 0;
 
-let getOrgs = async () => {
+let loadOrgs = async () => {
   let response = await fetch(`https://api.github.com/organizations?per_page=10&since=${orgID}`)
   return response.json();
 }
 
-getOrgs().then((orgs) => {
-  console.log(orgs)
+let getOrgs = () => {
 
-  orgs.forEach(org => {
+  loadOrgs().then((returnedOrgs) => {
+    console.log(returnedOrgs)
 
-    // document.querySelector('#list').innerHTML = org
-  })
+    returnedOrgs.forEach(returnedOrg => {
+      let org = `
+      <a class="org" href="${returnedOrg.url}">
+        <div class="avatar" style="background-image: url(${returnedOrg.avatar})"></div>
+        <h3>${returnedOrg.login}</h3>
+        <p>${returnedOrg.description || ''}</p>        
+      </a>
+      `;
 
-})
+      document.querySelector('.list_interior').innerHTML += org;
+    });
+
+    orgID = returnedOrgs[returnedOrgs.length - 1].id + 1;
+  });
+
+}
+
+
+document.querySelector('.loadMore').addEventListener('click', getOrgs)
+
+getOrgs();
+
+
